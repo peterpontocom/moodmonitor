a7fc6971033c0b12ce228dfef49d9b1e2c8ed1ea
peterpontocom <willpontocom07@gmail.com>
2023-06-28 22:58:13 +0100

    Estrutura do blog montada com sucesso, só falta algumas coisas

--- src/views/pages/main/blog/index.php
+++ src/views/pages/main/blog/index.php
@@ -1,3 +1,102 @@
 <?php
   include "../../../../components/pages/header.php";
 ?>
+<link rel="stylesheet" href="../../../../css/blog.css">
+
+<main>
+  <form class="search">
+    <div><input type="search" name="search" id="search" placeholder="Pesquisar"><button>Pesquisar</button></div>
+  </form>
+  <div class="slog">
+    Artigos escritos por profissionais
+  </div>
+    <a href="#" class="destach">
+      <h1>Descubra o Poder da Autocompaixão: Ame-se e Transforme sua Vida</h1>
+      <p>Mergulhe no mundo da autocompaixão e descubra como cultivar um relacionamento amoroso e compassivo consigo mesmo. Aprenda práticas simples e transformadoras para liberar a autocrítica, abraçar sua humanidade e desenvolver resiliência emocional. Descubra como a autocompaixão pode ser um catalisador para a cura, a felicidade e o crescimento pessoal. Seja seu maior aliado e comece a transformar sua vida hoje mesmo!</p>
+      <div class="hastag">
+        <span class="hastag-item">Autocompaixão</span>
+        <span class="hastag-item">Auto estima</span>
+        <span class="hastag-item">Transformação</span>
+      </div>
+    </a>
+    <section>
+      <article>
+        <div class="cover"></div>
+        <div class="date">Nome do autor - 19 Junho 2023</div>
+        <h1>Titulo do artigo</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis facilis voluptatum nesciunt asperiores commod.
+        </p>
+        <div class="hastag">
+          <span class="hastag-item">Algumacoisa</span>
+          <span class="hastag-item">Algumacoisa</span>
+          <span class="hastag-item">Alguma</span>
+        </div>
+      </article>
+      <article>
+        <div class="cover"></div>
+        <div class="date">Nome do autor - 19 Junho 2023</div>
+        <h1>Titulo do artigo</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis facilis voluptatum nesciunt asperiores commod.
+        </p>
+        <div class="hastag">
+          <span class="hastag-item">Algumacoisa</span>
+          <span class="hastag-item">Algumacoisa</span>
+          <span class="hastag-item">Alguma</span>
+        </div>
+      </article>
+      <article>
+        <div class="cover"></div>
+        <div class="date">Nome do autor - 19 Junho 2023</div>
+        <h1>Titulo do artigo</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis facilis voluptatum nesciunt asperiores commod.
+        </p>
+        <div class="hastag">
+          <span class="hastag-item">Algumacoisa</span>
+          <span class="hastag-item">Algumacoisa</span>
+          <span class="hastag-item">Alguma</span>
+        </div>
+      </article>
+      <article>
+        <div class="cover"></div>
+        <div class="date">Nome do autor - 19 Junho 2023</div>
+        <h1>Titulo do artigo</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis facilis voluptatum nesciunt asperiores commod.
+        </p>
+        <div class="hastag">
+          <span class="hastag-item">Algumacoisa</span>
+          <span class="hastag-item">Algumacoisa</span>
+          <span class="hastag-item">Alguma</span>
+        </div>
+      </article>
+      <article>
+        <div class="cover"></div>
+        <div class="date">Nome do autor - 19 Junho 2023</div>
+        <h1>Titulo do artigo</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis facilis voluptatum nesciunt asperiores commod.
+        </p>
+        <div class="hastag">
+          <span class="hastag-item">Algumacoisa</span>
+          <span class="hastag-item">Algumacoisa</span>
+          <span class="hastag-item">Alguma</span>
+        </div>
+      </article>
+      <article>
+        <div class="cover"></div>
+        <div class="date">Nome do autor - 19 Junho 2023</div>
+        <h1>Titulo do artigo</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis facilis voluptatum nesciunt asperiores commod.
+        </p>
+        <div class="hastag">
+          <span class="hastag-item">Algumacoisa</span>
+          <span class="hastag-item">Algumacoisa</span>
+          <span class="hastag-item">Alguma</span>
+        </div>
+      </article>
+    </section>
+</main>
