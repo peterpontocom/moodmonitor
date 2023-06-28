ab5d218414b2fb7d29a4a3874bb43e43380608f7
peterpontocom <willpontocom07@gmail.com>
2023-06-28 20:07:21 +0100

    Menu created

---
+++ src/components/pages/header.php
@@ -0,0 +1,25 @@
+<?php
+  include "../../../../components/header.php";
+?>
+<link rel="stylesheet" href="../../../../css/components/header.css">
+<link rel="stylesheet" href="../../../../css/components/main.css">
+
+<header class="d-flex justify-content-between align-items-center">
+  <a href="index.php" class="logo">
+    <img src="https://i.ibb.co/2KMbBK4/small-moodmonitor.png" alt="logo">
+  </a>
+  <nav>
+    <ul>
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">Sentindo</a></li>
+      <li><a href="#">Hábito</a></li>
+      <li><a href="#">Metas e Lembretes</a></li>
+      <li><a href="#">Progresso</a></li>
+      <li><a href="#">Comunidade</a></li>
+      <li><a href="#">Definições</a></li>
+    </ul>
+  </nav>
+  <div class="finishSession">
+    <button class="btn-finishSession">Terminar Sessão</button>
+  </div>
+</header>
---
+++ src/views/pages/main/blog/index.php
@@ -0,0 +1,3 @@
+<?php
+  include "../../../../components/pages/header.php";
+?>
