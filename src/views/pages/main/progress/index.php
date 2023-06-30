9168dbe5e43f2671c413dbe202c6da6836614eca
peterpontocom <willpontocom07@gmail.com>
2023-06-30 21:14:50 +0100

    let's go

--- src/views/pages/main/progress/index.php
+++ src/views/pages/main/progress/index.php
@@ -1,12 +1,14 @@
 <?php
 include "../../../../components/pages/header.php";
 include "../../../../database/connection.php";
+include "../../../../database/varification.php";
+
 ?>
 <link rel="stylesheet" href="../../../../css/diary.css">
 
 
-<ul class="diary-list">
-  <li><a href="#" class="justify-content-between"><span>Titulo</span> <span><button>Apagar</button><button>Editar</button></span></a></li>
+<ul class="diary-list d-none">
+  <li><a href="#"><span class="diary-list-title">Titulo do diário</span></a></li>
 </ul>
 <main>
   <div class="d-flex flex-column">
